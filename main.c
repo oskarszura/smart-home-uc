0d97398e520eecdd2b6f58b8c9b2c918beef507e
Oskar Szura <oskar.szura@gmail.com>
2018-03-18 22:03:35 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+const int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  float temperature = (voltage - .5) * 100;
+  
+  Serial.println(temperature);
+  delay(1000);
+}
