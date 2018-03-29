100268d1a16a351f2aa45d17e4c667704157b9e2
Oskar Szura <oskar.szura@gmail.com>
2018-03-29 09:37:29 +0200

    Add PIR sensor

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 const int sensorPin = A0;
+const int pirPin = 2;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(pirPin, INPUT); 
 }
 
 void loop() {
   int sensorVal = analogRead(sensorPin);
+  int presence = digitalRead(pirPin);
+
   float voltage = (sensorVal / 1024.0) * 5.0;
   float temperature = (voltage - .5) * 100;
   
   Serial.print("<");
   Serial.print(temperature);
+  Serial.print("|");
+  Serial.print(presence);
   Serial.print(">");
 
   delay(1000);
 }
+
